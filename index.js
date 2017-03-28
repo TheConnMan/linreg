52f49e71be972a8c77c4a1dea8a07df798fdda36
TheConnMan <bcconn2112@gmail.com>
2017-03-28 19:15:34 -0400

    Add expected line

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var moment = require('moment');
+var regression = require('regression');
 
 var app = express();
 
@@ -8,16 +9,28 @@ app.use(express.static('public'));
 var day = 24 * 3600 * 1000;
 var fiveMin = 300 * 1000;
 
+var trainingPeriod = 10;
+
 app.get('/data', function (req, res) {
   var dayAgo = new Date(Date.now() - day);
   var dates = Array.apply(null, Array(day / fiveMin)).map((i, period) => {
     return new Date(dayAgo.getTime() + period * fiveMin);
   });
-  var data = dates.map(date => {
+  var rawData = dates.map(date => {
     return {
       date: moment(date).format('YYYY-MM-DD HH:mm'),
       Raw: 100 * Math.random()
     };
+  });
+  var data = rawData.map((datum, i) => {
+    if (i >= trainingPeriod) {
+      var training = rawData.slice(i - trainingPeriod, i + 1).map((train, j) => {
+        return [j, train.Raw];
+      });
+      var result = regression('linear', training);
+      datum.Expected = result.points[trainingPeriod][1];
+    }
+    return datum;
   });
   res.send(data);
 });
