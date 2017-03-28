1ac5ef4d0838aee38f817b2b354741b3544761c2
TheConnMan <bcconn2112@gmail.com>
2017-03-28 19:20:50 -0400

    Add deviation lines

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var moment = require('moment');
 var regression = require('regression');
+var stats = require('stats-lite')
 
 var app = express();
 
@@ -10,6 +11,7 @@ var day = 24 * 3600 * 1000;
 var fiveMin = 300 * 1000;
 
 var trainingPeriod = 10;
+var stdevs = 3;
 
 app.get('/data', function (req, res) {
   var dayAgo = new Date(Date.now() - day);
@@ -24,11 +26,18 @@ app.get('/data', function (req, res) {
   });
   var data = rawData.map((datum, i) => {
     if (i >= trainingPeriod) {
-      var training = rawData.slice(i - trainingPeriod, i + 1).map((train, j) => {
-        return [j, train.Raw];
+      var rawVals = rawData.slice(i - trainingPeriod, i + 1).map(val => {
+        return val.Raw;
+      });
+      var training = rawVals.map((val, j) => {
+        return [j, val];
       });
       var result = regression('linear', training);
       datum.Expected = result.points[trainingPeriod][1];
+
+      var stdev = stats.stdev(rawVals);
+      datum.Upper = datum.Expected + stdevs * stdev;
+      datum.Lower = datum.Expected - stdevs * stdev;
     }
     return datum;
   });
