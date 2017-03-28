ad530a704f9232e97e4d897e26437104d2e631ac
TheConnMan <bcconn2112@gmail.com>
2017-03-28 19:06:14 -0400

    Add basic chart

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var express = require('express');
+var moment = require('moment');
+
 var app = express();
 
 app.use(express.static('public'));
+
+var day = 24 * 3600 * 1000;
+var fiveMin = 300 * 1000;
+
+app.get('/data', function (req, res) {
+  var dayAgo = new Date(Date.now() - day);
+  var dates = Array.apply(null, Array(day / fiveMin)).map((i, period) => {
+    return new Date(dayAgo.getTime() + period * fiveMin);
+  });
+  var data = dates.map(date => {
+    return {
+      date: moment(date).format('YYYY-MM-DD HH:mm'),
+      Raw: 100 * Math.random()
+    };
+  });
+  res.send(data);
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
