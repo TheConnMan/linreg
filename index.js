cd7698441b4b981229f108c099924540c1b6c4ca
TheConnMan <bcconn2112@gmail.com>
2017-03-28 21:01:55 -0400

    Improve base data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var day = 24 * 3600 * 1000;
 var fiveMin = 300 * 1000;
 
 var trainingPeriod = 10;
-var stdevs = 3;
+var stdevs = 2;
 
 app.get('/data', function (req, res) {
   var dayAgo = new Date(Date.now() - day);
@@ -21,23 +21,24 @@ app.get('/data', function (req, res) {
   var rawData = dates.map(date => {
     return {
       date: moment(date).format('YYYY-MM-DD HH:mm'),
-      Raw: 100 * Math.random()
+      Raw: 50 * Math.cos(2 * Math.PI * date.getTime() / (24 * 3600 * 1000)) + (20 * Math.random() - 0.5) + (Math.random() < 0.1 ? (100 * Math.random() - 50) : 0)
     };
   });
   var data = rawData.map((datum, i) => {
     if (i >= trainingPeriod) {
-      var rawVals = rawData.slice(i - trainingPeriod, i + 1).map(val => {
+      var rawVals = rawData.slice(i - trainingPeriod, i).map(val => {
         return val.Raw;
       });
       var training = rawVals.map((val, j) => {
         return [j, val];
       });
       var result = regression('linear', training);
-      datum.Expected = result.points[trainingPeriod][1];
+      datum.Expected = result.equation[0] * trainingPeriod + result.equation[1];
 
       var stdev = stats.stdev(rawVals);
       datum.Upper = datum.Expected + stdevs * stdev;
       datum.Lower = datum.Expected - stdevs * stdev;
+      datum.Deviation = datum.Upper < datum.Raw || datum.Lower > datum.Raw ? datum.Raw : null;
     }
     return datum;
   });
